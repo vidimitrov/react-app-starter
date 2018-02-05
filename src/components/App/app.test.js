82098806ef4d52209b0569d9367e034adf737206
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-02-05 07:02:45 +0200

    Fixing test environment WIP

--- src/components/App/app.test.js
+++ src/components/App/app.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import AppBar from 'material-ui/AppBar';
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
 import App from './app';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Example App component', () => {
+  test('renders without crashing', () => {
+    const app = shallow(<App />);
+    expect(app.exists()).toEqual(true);
+  });
+
+  test('renders an AppBar', () => {
+    const app = shallow(<App />);
+    expect(app.exists()).toEqual(true);
+  });
+
+  test('renders a List of items');
+
+  test('renders 3 example items');
 });
