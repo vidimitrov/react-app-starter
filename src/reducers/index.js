1bc0d70686c2cad5737dfcb40008811a34f0175a
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-04-13 15:01:54 +0300

    Return only the reducer functions without  combining them

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
-import {combineReducers} from 'redux';
-
 // Reducers
 import {todos, visibilityFilter} from './todos';
 
 // Combine them all and return them as a single reducer
-export default combineReducers({
+export {
   todos,
   visibilityFilter,
-});
+};
 
